a287ed554fd4b40e075da4f9479b11f2eb5a354f
Allyson Athyrson <allysonathyrson.work@gmail.com>
2024-09-20 14:05:21 -0300

    utils (feat): check if it's a positive number

---
+++ src/utils/onlyPositiveNumber.ts
@@ -0,0 +1,8 @@
+export const onlyPositiveNumber = (
+  numberValue: number | null
+): number | null => {
+  if (numberValue === null) return null;
+  if (numberValue <= 0) return 0;
+
+  return numberValue;
+};
