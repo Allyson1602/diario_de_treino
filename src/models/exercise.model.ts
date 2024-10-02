ab6ff192cbf1c383cbe539d93d53517338e48017
Allyson Athyrson <allysonathyrson.work@gmail.com>
2024-10-02 09:50:45 -0300

    models (refactor): keep exercise attributes optional

--- src/models/exercise.model.ts
+++ src/models/exercise.model.ts
@@ -1,9 +1,9 @@
 import { MuscleModel } from "./muscle.model";
 
 export interface ExerciseModel {
-  anotation: string;
-  repetitions: number;
-  weight: number;
-  timer: string;
-  muscles: MuscleModel[];
+  anotation?: string;
+  repetitions?: number;
+  weight?: number;
+  timer?: string;
+  muscles?: MuscleModel[];
 }
