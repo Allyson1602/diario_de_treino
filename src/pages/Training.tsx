f0b24488ca8a74e3fb92c6eb6b93f96fe36f06af
Allyson Athyrson <allysonathyrson.work@gmail.com>
2024-10-17 10:59:28 -0300

    pages (feat): show modal when have only one exercise on training

--- src/pages/Training.tsx
+++ src/pages/Training.tsx
@@ -1,11 +1,20 @@
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { Center, HStack, ScrollView, Text, useTheme, VStack } from "native-base";
+import {
+  Button,
+  Center,
+  HStack,
+  Modal,
+  ScrollView,
+  Text,
+  useDisclose,
+  useTheme,
+  VStack,
+} from "native-base";
 import { FunctionComponent } from "react";
 import { cancelAnimation, SharedValue, useSharedValue, withTiming } from "react-native-reanimated";
 import { Card } from "../components/Card";
 import { CustomAnimated } from "../components/ui/CustomAnimated";
-import { useExercise } from "../hooks/useExercise";
 import { useTraining } from "../hooks/useTraining";
 import { ExerciseModel } from "../models/exercise.model";
 import { RootStackParamList } from "../navigation";
@@ -14,8 +23,8 @@ type TrainingProps = NativeStackScreenProps<RootStackParamList, "Training", "Roo
 
 export const Training: FunctionComponent<TrainingProps> = ({ navigation }) => {
   const theme = useTheme();
-  const exerciseHook = useExercise();
   const trainingHook = useTraining();
+  const { isOpen, onClose, onOpen } = useDisclose(false);
   const scaleStartWorkout = useSharedValue(1);
   const scaleAddWorkout = useSharedValue(1);
 
@@ -32,91 +41,142 @@ export const Training: FunctionComponent<TrainingProps> = ({ navigation }) => {
     defineAnimationOnPress(scaleAddWorkout);
   };
 
-  const handleRemoveExercise = () => {
-    console.log("remove");
+  const handleRemoveExercise = (exerciseSelected: ExerciseModel) => {
+    const exercises = trainingHook.trainingActive?.exercises;
+
+    if (exercises?.length === 1) {
+      onOpen();
+      return;
+    }
+
+    trainingHook.removeExercise(exerciseSelected);
   };
 
   const handlePressStartWorkout = () => {
+    const firstExercise = trainingHook.trainingActive?.exercises[0];
+
     defineAnimationOnPress(scaleStartWorkout);
+
+    if (firstExercise) {
+      trainingHook.setExerciseActive(firstExercise);
+      navigation.navigate("Workout");
+    }
   };
 
   const handlePressExerciseItem = (exerciseItem: ExerciseModel) => {
-    exerciseHook.setExerciseActive(exerciseItem);
+    trainingHook.setExerciseActive(exerciseItem);
     navigation.navigate("Workout");
   };
 
+  const handleDeleteTraining = () => {
+    trainingHook.removeData(trainingHook.trainingActive!).then((status) => {
+      if (status) {
+        trainingHook.setTrainingActive(null);
+        navigation.navigate("Home");
+      }
+    });
+  };
+
   return (
-    <VStack
-      safeArea
-      pt={"6"}
-      pb={"6"}
-      flex={1}
-      background={{
-        linearGradient: {
-          colors: ["#FFACBA", "#EEEEEE"],
-          start: [2, 0],
-          end: [0, 1],
-        },
-      }}
-      justifyContent={"space-between"}
-    >
-      <VStack safeArea pt={"6"} flex={"1"} mb={"10"}>
-        <HStack alignItems={"center"} justifyContent={"space-between"} px={"6"}>
-          <Text color={"text.500"} fontWeight={"medium"}>
-            Exercícios
-          </Text>
+    <>
+      <VStack
+        safeArea
+        pt={"6"}
+        pb={"6"}
+        flex={1}
+        background={{
+          linearGradient: {
+            colors: ["#FFACBA", "#EEEEEE"],
+            start: [2, 0],
+            end: [0, 1],
+          },
+        }}
+        justifyContent={"space-between"}
+      >
+        <VStack safeArea pt={"6"} flex={"1"} mb={"10"}>
+          <HStack alignItems={"center"} justifyContent={"space-between"} px={"6"}>
+            <Text color={"text.500"} fontWeight={"medium"}>
+              Exercícios
+            </Text>
 
-          <CustomAnimated.IconButton
-            icon={<Ionicons name="add-circle" size={30} color={theme.colors.primary[600]} />}
+            <CustomAnimated.IconButton
+              icon={<Ionicons name="add-circle" size={30} color={theme.colors.primary[600]} />}
+              rounded={"full"}
+              p={"0.5"}
+              onPress={handlePressNewWorkout}
+              style={{ transform: [{ scale: scaleAddWorkout }] }}
+              variant={"unstyled"}
+            />
+          </HStack>
+
+          <ScrollView
+            contentContainerStyle={{
+              gap: 8,
+              paddingVertical: 8,
+              paddingHorizontal: 24,
+            }}
+            showsVerticalScrollIndicator={false}
+          >
+            {trainingHook.trainingActive?.exercises.map((exerciseItem) => (
+              <Card.Container
+                key={exerciseItem.id}
+                onPress={() => handlePressExerciseItem(exerciseItem)}
+              >
+                <HStack flexGrow={1} space={"4"}>
+                  <VStack justifyContent={"space-between"} space={"4"} flex={"1"}>
+                    <Card.Title text={exerciseItem.name} />
+
+                    <Card.MuscleChips muscleNames={exerciseItem.muscles} />
+                  </VStack>
+
+                  <Card.TrashButtonIcon onRemove={() => handleRemoveExercise(exerciseItem)} />
+                </HStack>
+              </Card.Container>
+            ))}
+          </ScrollView>
+        </VStack>
+
+        <Center>
+          <CustomAnimated.Button
             rounded={"full"}
-            p={"0.5"}
-            onPress={handlePressNewWorkout}
-            style={{ transform: [{ scale: scaleAddWorkout }] }}
-            variant={"unstyled"}
-          />
-        </HStack>
-
-        <ScrollView
-          contentContainerStyle={{
-            gap: 8,
-            paddingVertical: 8,
-            paddingHorizontal: 24,
-          }}
-          showsVerticalScrollIndicator={false}
-        >
-          {trainingHook.trainingActive?.exercises.map((exerciseItem) => (
-            <Card.Container
-              key={exerciseItem.id}
-              onPress={() => handlePressExerciseItem(exerciseItem)}
-            >
-              <HStack flexGrow={1} space={"4"}>
-                <VStack justifyContent={"space-between"} space={"4"} flex={"1"}>
-                  <Card.Title text={exerciseItem.name} />
-
-                  <Card.MuscleChips muscleNames={exerciseItem.muscles} />
-                </VStack>
-
-                <Card.TrashButtonIcon onRemove={handleRemoveExercise} />
-              </HStack>
-            </Card.Container>
-          ))}
-        </ScrollView>
+            size={"lg"}
+            px={"8"}
+            style={{ transform: [{ scale: scaleStartWorkout }] }}
+            _text={{
+              fontSize: "3xl",
+            }}
+            onPress={handlePressStartWorkout}
+          >
+            Iniciar
+          </CustomAnimated.Button>
+        </Center>
       </VStack>
 
-      <Center>
-        <CustomAnimated.Button
-          rounded={"full"}
-          size={"lg"}
-          px={"8"}
-          style={{ transform: [{ scale: scaleStartWorkout }] }}
-          _text={{
-            fontSize: "3xl",
-          }}
-          onPress={handlePressStartWorkout}
-        >
-          Iniciar
-        </CustomAnimated.Button>
-      </Center>
-    </VStack>
+      <Modal isOpen={isOpen} onClose={onClose} px={"6"}>
+        <Modal.Content borderRadius={22} width={"full"}>
+          <Modal.CloseButton
+            _icon={{
+              size: "xl",
+            }}
+          />
+          <Modal.Body>
+            <Text pt={"12"} fontSize={"lg"} textAlign={"center"}>
+              Se você remover esse exercício, o treino será apagado!
+            </Text>
+            <Text fontSize={"lg"} pt={"8"} pb={"2"} textAlign={"center"}>
+              Deseja continuar?
+            </Text>
+            <Button
+              onPress={handleDeleteTraining}
+              size={"lg"}
+              backgroundColor={"danger.600"}
+              _text={{ fontSize: "lg" }}
+            >
+              deletar treino
+            </Button>
+          </Modal.Body>
+        </Modal.Content>
+      </Modal>
+    </>
   );
 };
